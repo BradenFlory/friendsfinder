fd7cffec11b5f0bc9c69df047e4604948fef19f5
Brado <bradenflory2015@gmail.com>
2019-05-06 20:44:56 -0600

    Here is my friend finder app

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friend = [
+    {
+        name: "Braden Flory",
+        photo: "https://shop.wilson.com/media/catalog/product/cache/38/image/9df78eab33525d08d6e5fb8d27136e95/7/8/78adcafa2789c1170e658b69fd5c334d0672ae08_wtf1100_1.jpg",
+        score: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+
+    },
+    {
+        name: "Lucifer the Devil",
+        photo: "https://shop.wilson.com/media/catalog/product/cache/38/image/9df78eab33525d08d6e5fb8d27136e95/7/8/78adcafa2789c1170e658b69fd5c334d0672ae08_wtf1100_1.jpg",
+        score: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    },
+];
+
+module.exports = friend;
